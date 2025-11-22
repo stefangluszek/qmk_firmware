0584e4084f01bb9ae5ced1881ebea11cd2967b31
Stefan Gluszek <stefang@kahoot.com>
2025-11-22 15:37:02 +0100

    Removed unused encoder map

--- keyboards/fatotesa/keymaps/via/keymap.c
+++ keyboards/fatotesa/keymaps/via/keymap.c
@@ -3,14 +3,6 @@
 
 #include QMK_KEYBOARD_H
 
-#if defined(ENCODER_MAP_ENABLE)
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
-    [1] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
-};
-#endif
-
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4,                                                                          KC_END, KC_INSERT, KC_DELETE, KC_KB_MUTE,
